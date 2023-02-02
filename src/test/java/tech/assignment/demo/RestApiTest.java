b49950e9de7adef1eff212f564edcb8d539bb860
HogmanGlenn <ghogman19@student.oulu.fi>
2023-02-02 20:29:27 +0200

    Added 2 tests

--- src/test/java/tech/assignment/demo/RestApiTest.java
+++ src/test/java/tech/assignment/demo/RestApiTest.java
@@ -7,21 +7,19 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import tech.assignment.demo.service.CountryService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -34,14 +32,28 @@ public class RestApiTest {
     @Autowired
     private ObjectMapper mapper;
 
+    @Autowired
+    private CountryService service;
+    
     @Test
     @Order(1)
-    public void canGetAll() throws JsonProcessingException, Exception {
-        this.mockMvc
-        .perform(get("/country/countries"))
+    public void canGetIndividual() throws JsonProcessingException, Exception {
+        mockMvc
+        .perform(get("/api/countries?country=afghanistan"))
         .andExpect(status().isOk())
         .andDo(print())
-        .andExpect(content().string(containsString("Finland")));
+        .andExpect(jsonPath("$[0].name").value("Afghanistan"));
     }
-    
+
+    @Test
+    @Order(2)
+    public void canGetAllAndCheckFirstCountry() throws JsonProcessingException, Exception {
+        mockMvc
+        .perform(get("/api/countries"))
+        .andExpect(status().isOk())
+        .andDo(print())
+        .andExpect(jsonPath("$[0].name").value("Afghanistan"))
+        .andExpect(jsonPath("$[0].countryCode").value("AF"));
+
+    }
 }
